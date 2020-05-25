3ee1a71fc4ab37381df60e5f83039f40a0f38fce
s13382570 <s13488941815>
2020-05-25 17:50:40 +0800

    permision

--- app/src/main/java/sc/hotpot/update/TestActivity.java
+++ app/src/main/java/sc/hotpot/update/TestActivity.java
@@ -13,7 +13,7 @@ public class TestActivity  extends Activity {
         setContentView(R.layout.layout);
         findViewById(R.id.button).setOnClickListener((v)->{
             DownLoadUtil util= DownLoadUtil.getInstance(this);
-            util.startDownload("test","upload.apk","sdfsdf");
+            util.startDownload("fileurl","upload.apk","sdfsdf");
 //            util.startDownload("https://dss2.bdstatic.com/6Ot1bjeh1BF3odCf/it/u=1603468086,1674922376&fm=85&app=92&f=JPEG?w=121&h=75&s=E0C3D71E4B407CC840CD0D54030050F2","upload.apk","sdfsdf");
 
         });
